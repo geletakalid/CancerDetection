8af80a0351554ff2911cbe8a5e93710a4fd7dde9
kalid geleta <kalidgeleta28@gmail.com>
2024-12-25 12:00:45 +0300

    ui for the server

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <Navbar/>
+  );
+}
+
+export default App;
